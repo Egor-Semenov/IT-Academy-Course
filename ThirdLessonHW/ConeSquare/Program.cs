a6e71f575a51a5b0087a9184dbf136654f2bd947
Egor-Semenov <egor123semenov@gmail.com>
2021-07-24 00:26:39 +0300

    ThirdLessonHW

---
+++ ThirdLessonHW/ConeSquare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConeSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI;
+            double l, r;
+
+            Console.WriteLine("Введите радиус R: ");
+            r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите образующую L: ");
+            l = double.Parse(Console.ReadLine());
+
+            double coneSquare = GetConeSquare(pi, r, l);
+
+            Console.WriteLine($"Площадь конуса равна {coneSquare}");
+        }
+
+        static double GetConeSquare(double pi, double r, double l)
+        {
+            return pi * r * (r + l); 
+        }
+    }
+}
