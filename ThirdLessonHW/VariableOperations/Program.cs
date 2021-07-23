fdab54d6760af79122fdd4e1855c2f82b4e5887b
Egor-Semenov <egor123semenov@gmail.com>
2021-07-24 01:42:36 +0300

    ThirdLessonHW

---
+++ ThirdLessonHW/VariableOperations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariableOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine($"Значение x после (x += y - x++ * z) : {x}");
+            z = --x - y * 5;
+            Console.WriteLine($"Значение z после (z = --x - y * 5) : {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"Значение y после (y /= x + 5 % z) : {y}");
+            z = x++ + y * 5;
+            Console.WriteLine($"Значение z после (z = x++ + y * 5) : {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"Значение x после (x = y - x++ * z) : {x}");
+
+        }
+    }
+}
