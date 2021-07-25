10be25ed9e675ba8d6be29c421af0eece091ecfb
Egor-Semenov <egor123semenov@gmail.com>
2021-07-25 18:06:00 +0300

    ThirdLessonHW

---
+++ ThirdLessonHW/AdditionalTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел для нахождения НОД: ");
+            int number = int.Parse(Console.ReadLine());
+
+            double[] arr = new double[number];
+
+            for(int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Введите число {i + 1} : ");
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+
+            int iteration = 1;
+            double NOD = arr[0];
+            
+            while(iteration < number)
+            {
+                double c = NOD;
+
+                while (NOD == c)
+                {
+                    if ((NOD > arr[iteration]) && (NOD % arr[iteration]) == 0)
+                    {
+                        NOD = arr[iteration];
+                    }
+                    else
+                    {
+                        NOD = NOD % arr[iteration];
+                    }
+
+                    if ((arr[iteration] > NOD) && (arr[iteration] % NOD) == 0)
+                    {
+                        c++;
+                    }
+                    else
+                    {
+                        NOD = arr[iteration] % NOD;
+                    }
+                }
+
+                iteration++;
+
+            }
+
+            Console.WriteLine($"НОД : {NOD}");
+        }
+    }
+}
