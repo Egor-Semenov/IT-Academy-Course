e8c75ec12e8d97a331acca88eb9fdefd00e5720c
Egor-Semenov <egor123semenov@gmail.com>
2021-08-09 04:06:48 +0300

    SeventhLessonHW

---
+++ SeventhLessonHW/FirastTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FirastTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Введите новое значение элемента № {i+1}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вывод отсортированного массива:");
+
+            foreach(var a in arr)
+            {
+                Console.Write($"{a} ");
+            }
+        }
+    }
+}
---
+++ SeventhLessonHW/SecondTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3,3] { { 5, 1, 2 }, { 7, 9, 3 } ,{ 6 , 1 , 8} };
+
+            int rows = arr.GetUpperBound(0) + 1;
+            int columns = arr.Length / rows;
+
+            for(int i = 0; i < rows; i++)
+            {
+                int max = arr[i, 0];
+                for(int j = 1; j < columns; j++)
+                {
+                    if(arr[i,j] > max)
+                    {
+                        max = arr[i, j];
+                    }
+                }
+                Console.WriteLine($"Максимальный элемент в ряду №{i + 1} : {max}");
+            }
+        }
+    }
+}
