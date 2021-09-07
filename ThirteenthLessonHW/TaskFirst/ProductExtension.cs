def6bbda6ba21bab88aa0fd8c479b90823bd3757
Egor-Semenov <egor123semenov@gmail.com>
2021-09-08 01:44:35 +0300

    ThirteenthLessonHW

---
+++ ThirteenthLessonHW/TaskFirst/ProductExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFirst
+{
+    public static class ProductExtension
+    {
+        public static void Sort(this Product[] products)
+        {
+            for (int i = 1; i < products.Length; i++)
+            {
+                for (int j = 0; j < products.Length - 1; j++)
+                {
+                    if (string.Compare(products[j].ShopName, products[j + 1].ShopName) > 0)
+                    {
+                        var temp = products[j];
+                        products[j] = products[j + 1];
+                        products[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void SearchShopItems(this Product[] products , string shopName)
+        {
+            int count = 0;
+            for(int i = 0; i < products.Length; i++)
+            {
+                if (products[i].ShopName == shopName)
+                {
+                    
+                    Console.WriteLine(products[i].ProductName);
+                    count++;
+                }
+            }
+
+            if(count == 0)
+            {
+                throw new MyException($"Магазина с названием {shopName} не существует");
+            }
+        }
+    }
+}
---
+++ ThirteenthLessonHW/TaskFirst/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TaskFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[5];
+
+            Product product1 = new Product() {ShopName = "Lemon", ProductName = "Fork" };
+
+            Product product2 = new Product() { ShopName = "Green", ProductName = "Spoon" };
+
+            Product product3= new Product() { ShopName = "Green", ProductName = "Phone" };
+
+            Product product4 = new Product() { ShopName = "Lemon", ProductName = "TV" };
+
+            Product product5 = new Product() { ShopName = "Apple", ProductName = "Food" };
+
+            products[0] = product1;
+            products[1] = product2;
+            products[2] = product3;
+            products[3] = product4;
+            products[4] = product5;
+
+            
+
+            products.Sort();
+
+            foreach (var item in products)
+            {
+
+                Console.WriteLine(item.ProductName + " " + item.ShopName);
+                
+            }
+
+            Console.WriteLine("");
+
+            try
+            {
+                products.SearchShopItems("Gren");
+            }
+            catch(MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("");
+
+            try
+            {
+                products.SearchShopItems("Green");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        
+    }
+}
+
+
