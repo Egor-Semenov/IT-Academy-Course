38062cbd1bd0462b3d979a0ad5a23e356cb4878b
Egor-Semenov <egor123semenov@gmail.com>
2021-09-17 18:45:54 +0300

    FifteenthLessonHW

---
+++ FifteenthLessonHW/FifteenthLessonHW/MyFileSearcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenthLessonHW
+{
+    public class MyFileSearcher
+    {
+        public void SearchAndArhivate(DirectoryInfo directoryInfo, string fileName)
+        {
+            FileInfo searchFile = null;
+
+            searchFile = RecursiveSearch(directoryInfo, fileName, searchFile);
+
+            if(searchFile != null)
+            {
+                var archive = Path.Combine(searchFile.Directory.FullName, Path.GetFileNameWithoutExtension(searchFile.FullName) + ".zip");
+
+                using (var fileStream = new FileStream(archive, FileMode.Create))
+
+                using (ZipArchive zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Create)) 
+                    zipArchive.CreateEntryFromFile(searchFile.FullName, searchFile.Name);
+
+                Console.WriteLine($"Файл заархивирован. Путь к архиву : {archive}");
+            }
+            else
+            {
+                throw new FileNotFoundException("Файл не найден");
+            }
+
+        }
+
+        FileInfo RecursiveSearch(DirectoryInfo directoryInfo, string fileName, FileInfo searchFile)
+        {
+            if (searchFile != null) return searchFile;
+
+            foreach(var file in directoryInfo.GetFiles())
+            {
+                if(fileName.Equals(file.Name))
+                {
+                   return file;
+                }
+            }
+
+            var directories = directoryInfo.GetDirectories();
+
+            if(directories.Any())
+            {
+                foreach(var directory in directories)
+                {
+                    searchFile = RecursiveSearch(directory, fileName, searchFile);
+                    if (searchFile != null) return searchFile;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ FifteenthLessonHW/FifteenthLessonHW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FifteenthLessonHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFileSearcher searcher = new MyFileSearcher();
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"D:\Курсы C#");
+
+            searcher.SearchAndArhivate(directoryInfo, "Text.txt");
+        }
+
+        
+    }
+
+   
+}
